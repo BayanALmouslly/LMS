e35d5df6574e36857905e88d5d1aaf68d46bcd1f
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-06-11 20:55:40 +0300

    student exam

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -19,4 +19,46 @@ StudentServies(){
 support(){
   this.router.navigate(['/pages/help/support'])
 }
+home(){
+  this.router.navigate(['/pages/homepage/start'])
+
+}
+coursedesc(){
+  this.router.navigate(['/pages/courses'])
+
+}
+coursePolicy(){
+  this.router.navigate(['/pages/coursePolicy'])
+
+}
+advertising(){
+  this.router.navigate(['/pages/advertising'])
+
+}
+educationalunit(){
+  this.router.navigate(['/pages/educationalunit'])
+
+}
+homeworks(){
+  this.router.navigate(['/pages/homeworks'])
+
+}
+discussions(){
+  this.router.navigate(['/pages/discussions'])
+
+}
+CourseEvaluation(){
+  this.router.navigate(['/pages/CourseEvaluation'])
+
+}
+exam(){
+  this.router.navigate(['/pages/exam'])
+
+}
+help(){
+  this.router.navigate(['/pages/help'])
+
+}
+
+
 }
